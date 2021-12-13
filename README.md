33aea8eb8e56d523f92a60bcc45c04fecaebae71
Jorge Reyes <jorge_irving_15@hotmail.com>
2021-12-13 20:38:10 -0300

    Init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# **Desafio Clases ES6**
+Desafio 09, Modulo 03 Introduccion Javascript
+
+## **Capitulos**
+* ES6
+* Variables let y const en ES6
+* Transformado ES6 a ES5
+* Objetos en ES6
+* Clases a partir de un diagrama UML
+* Implementar la modularidad de ES6
---
+++ src/Impuestos.js
@@ -0,0 +1,29 @@
+export default class Impuestos {
+
+    /** Constructor 
+     * @param {number} monto_bruto_anual 
+     * @param {number} deducciones 
+    */
+
+    constructor(monto_bruto_anual, deducciones) {
+        this._monto_bruto_anual = monto_bruto_anual;
+        this._deducciones = deducciones;
+    }
+
+    get monto_bruto_anual() {
+        return this._monto_bruto_anual;
+    }
+
+    set monto_bruto_anual(monto_bruto_anual) {
+        this._monto_bruto_anual = monto_bruto_anual;
+    }
+
+    get deducciones() {
+        return this._deducciones;
+    }
+
+    set deducciones(deducciones) {
+        this._deducciones = deducciones;
+    }
+
+}
